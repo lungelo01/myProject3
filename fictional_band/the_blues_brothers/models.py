415b098093f5cb52da319878721381499a9dbca0
lungelo01 <69298406+lungelo01@users.noreply.github.com>
2025-07-12 00:18:43 +0200

    Add docstrings to models.py

--- fictional_band/the_blues_brothers/models.py
+++ fictional_band/the_blues_brothers/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Member(models.Model):
+    """The project model with firstname, lastname, message and date data and returns firstname and lastname."""
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     date = models.DateField()
